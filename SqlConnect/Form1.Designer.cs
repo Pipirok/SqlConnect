eadc92fad2d9ecbb44d7844cbc9b81c9cebce709
pipirok <pipirok@vivaldi.net>
2022-11-03 12:05:14 +0400

    Add project files.

---
+++ SqlConnect/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SqlConnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSqlAction = new System.Windows.Forms.Button();
+            this.labelConnectionState = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSqlAction
+            // 
+            this.buttonSqlAction.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSqlAction.Location = new System.Drawing.Point(389, 88);
+            this.buttonSqlAction.Name = "buttonSqlAction";
+            this.buttonSqlAction.Size = new System.Drawing.Size(180, 60);
+            this.buttonSqlAction.TabIndex = 0;
+            this.buttonSqlAction.Text = "Connect";
+            this.buttonSqlAction.UseVisualStyleBackColor = true;
+            this.buttonSqlAction.Click += new System.EventHandler(this.buttonSqlAction_Click);
+            // 
+            // labelConnectionState
+            // 
+            this.labelConnectionState.AutoSize = true;
+            this.labelConnectionState.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelConnectionState.Location = new System.Drawing.Point(36, 101);
+            this.labelConnectionState.Name = "labelConnectionState";
+            this.labelConnectionState.Size = new System.Drawing.Size(249, 32);
+            this.labelConnectionState.TabIndex = 1;
+            this.labelConnectionState.Text = "Connection closed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 277);
+            this.Controls.Add(this.labelConnectionState);
+            this.Controls.Add(this.buttonSqlAction);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSqlAction;
+        private System.Windows.Forms.Label labelConnectionState;
+    }
+}
+
---
+++ SqlConnect/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlConnect
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn = new SqlConnection("Server=localhost; Database=Northwind; Integrated Security=true;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSqlAction_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                buttonSqlAction.Text = "Disconnect";
+                labelConnectionState.Text = "Connection open";
+                return;
+            }
+            conn.Close();
+            buttonSqlAction.Text = "Connect";
+            labelConnectionState.Text = "Connection closed";
+        }
+    }
+}
